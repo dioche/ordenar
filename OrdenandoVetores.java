720129c23a9f5e1d5c60250950695222f807a4d9
dioche <rockdi_sk8@hotmail.com>
2019-04-14 17:08:26 -0300

    Adicionando OrdenandoVetores e removendo QuickSort

--- OrdenandoVetores.java
+++ OrdenandoVetores.java
@@ -1,36 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package aplicativoOrdenacao;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class OrdenandoVetores {
 
     public static void main(String args[]) {
         //Criando a lista encadeada 
-        List<Integer> numeros = new ArrayList();
+        List<Integer> numeros = new ArrayList<Integer>();
 
         //Adicionando numeros para a lista
-        for (int i = 0; i < 100000; i++) {
+        for (int i = 0; i < 10000; i++) {
             numeros.add(i);
         }
 
         //Embaralhando a Lista utilizando método da API java Collections  
         Collections.shuffle(numeros);
 
-        System.out.println(" --------------------");
-        System.out.println("|Numeros Desordenados|");
-        System.out.println(" --------------------");
+//        System.out.println(" --------------------");
+//        System.out.println("|Numeros Desordenados|");
+//        System.out.println(" --------------------");
 
         //Imprimindo a lista utilizando o método forEach que contempla a classe List 
-        numeros.forEach(System.out::println);
+//        numeros.forEach(System.out::println);
 
         //transformando a lista em um arr11ay para simplificar o sort
         int[] arrayNumeros = new int[numeros.size()];
@@ -39,13 +32,15 @@ public class OrdenandoVetores {
             arrayNumeros[i++] = e;
         }
         int opcao;
-        do{
-        System.out.println(" \nEscolha o método: \n\n 1-Selection sort \n 2-Bubble Sort \n 3-Quick Sort \n 0-Para sair do sistema");
+//        do{
+//        System.out.println(" \nEscolha o método: \n\n 1-Selection sort \n 2-Bubble Sort \n 0-Para sair do sistema");;;
+       
 
        
 
-        Scanner entrada = new Scanner(System.in);
-        opcao = entrada.nextInt();
+//        Scanner entrada = new Scanner(System.in);
+//        opcao = entrada.nextInt();
+        opcao = 2;
 
         
            switch (opcao) {
@@ -53,10 +48,10 @@ public class OrdenandoVetores {
                     //Metodo da API java que arrayNumerosorna o tempo gasto para executar o trecho de código em millisegundos
                     long inicialSelection = System.currentTimeMillis();
                     //Selection Sort
-                    for (int fixo = 0; fixo < 99998; fixo++) {
+                    for (int fixo = 0; fixo < 9998; fixo++) {
                         int menor = fixo;
                         
-                        for (int s = menor + 1; s < 99999; s++) {
+                        for (int s = menor + 1; s < 9999; s++) {
                             if (arrayNumeros[s] < arrayNumeros[menor]) {
                                 menor = s;
                             }
@@ -73,7 +68,7 @@ public class OrdenandoVetores {
                     System.out.println("|Numeros ordenados por Selection Sort |");
                     System.out.println(" ----------------------------------");
                     int s;
-                    for (s = 0; s < 99999; s++) {
+                    for (s = 0; s < 9999; s++) {
                         System.out.println(arrayNumeros[s]);
                     }   System.out.println(" -------------------------------------------------------------");
                     System.out.println("|Tempo de execução do método Selection Sort:" + " " + decorridoSelection + " " + "Milisegundos   |");
@@ -85,8 +80,8 @@ public class OrdenandoVetores {
                     //Bubble Sort
                     int aux;
                     int u;
-                    for (u = 0; u < 99999; u++) {
-                        for (int K = 0; K < 99998; K++) {
+                    for (u = 0; u < 9999; u++) {
+                        for (int K = 0; K < 9998; K++) {
                             if (arrayNumeros[K] > arrayNumeros[K + 1]) {
                                 aux = arrayNumeros[K];
                                 arrayNumeros[K] = arrayNumeros[K + 1];
@@ -101,34 +96,17 @@ public class OrdenandoVetores {
                     System.out.println(" ----------------------------------");
                     System.out.println("|Numeros ordenados por Bubble Sort |");
                     System.out.println(" ----------------------------------");
-                    for (u = 0; u < 99999; u++) {
+                    for (u = 0; u < 9999; u++) {
                         System.out.println(arrayNumeros[u]);
                     }   System.out.println(" -------------------------------------------------------------");
                     System.out.println("|Tempo de execução do método Bubble Sort:" + " " + decorridoBubble + " " + "Milisegundos   |");
                     System.out.println(" -------------------------------------------------------------");
                     break;
-                case 3:
-                    //Metodo da API java que arrayNumerosorna o tempo gasto para executar o trecho de código em millisegundos
-                    long inicio = System.currentTimeMillis();
-                    //Chamada ao metodo QuickSort
-                    QuickSort.quickSort(arrayNumeros, 0, arrayNumeros.length);
-                    //Contagem ao finalizar o método
-                    long decorridoQuick = System.currentTimeMillis() - inicio;
-                    //imprimindo o vetor
-                    System.out.println(" ----------------------------------");
-                    System.out.println("|Numeros ordenados por QuickSort |");
-                    System.out.println(" ----------------------------------");
-                    int q;
-                    for (q = 0; q < 99999; q++) {
-                        System.out.println(arrayNumeros[q] + " ");
-                    }   System.out.println(" -------------------------------------------------------------");
-                    System.out.println("|Tempo de execução do método QuickSort:" + " " + decorridoQuick + " " + "Milisegundos   |");
-                    System.out.println(" -------------------------------------------------------------");
-                    break;
-                default: System.err.println("Fim da execução");
+                
+                default: System.err.println("ERRO-666; Dígito inválido. Não foi possível Sortear essa bagaça!");
                     break;
             }
-        }while(opcao != 0);
+//        }while(opcao != 0);
         
         System.exit(0);
     }
